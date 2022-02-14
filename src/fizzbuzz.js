ce2a00bdcb900100d49d8b081219e364f0cd75e6
Dylan Tangamen <tangamendylan@mail.com>
2022-02-14 18:59:31 +0100

    ajout fizzbuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,12 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
+    let result ="";
+	if (n%3==0) {
+		result+="Fizz";
+	}
+	if (n%5==0) {
+		result+="Buzz"
     return "fizzbuzz"
+	}
 }
 
 module.exports = fizzBuzz
